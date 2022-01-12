c62d5d5343f5c4f988c2b837efc26c855970b7fa
Jessica Liu <msjessicavliu@gmail.com>
2022-01-11 19:35:05 -0800

    Move all front-end code under client dir

---
+++ client/src/App.tsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+import { Link, Outlet } from 'react-router-dom';
+import { isAsExpression } from 'typescript';
+
+const initContextValue: any = null;
+export const LocationContext = React.createContext(initContextValue);
+
+export const App = () => {
+	const [msg, setMsg] = useState('');
+	useEffect(() => {
+		console.log('here')
+		axios.get('/api/hello')
+			.then((res: { data: React.SetStateAction<string>; }) => { console.log('returned', res.data); setMsg(res.data)} )
+	}, [])
+
+	
+	// const [locationForMap, setLocationForMap] = useState(null);
+
+	// return (
+	// 	<>
+	// 		<LocationContext.Provider value={{ locationForMap, setLocationForMap }}>
+	// 		<link rel="stylesheet" href="https://unpkg.com/tachyons@4/css/tachyons.min.css"></link>
+	// 		<ul>
+	// 			<li><Link to="/">Home</Link></li>
+	// 			<li><Link to="/itineraries">Itineraries</Link></li>
+	// 			<li><Link to="/locations">Locations</Link></li>
+	// 		</ul>
+	// 		<Outlet/>
+	// 		</LocationContext.Provider>
+	// 	</>
+	// );
+
+	return (
+		<> 
+			{msg} 
+		</>
+	)
+}
+
+{/* <link rel="stylesheet" href="https://unpkg.com/tachyons@4/css/tachyons.min.css"></link>
+<ul>
+	<li><Link to="/">Home</Link></li>
+	<li><Link to="/itineraries">Itineraries</Link></li>
+	<li><Link to="/locations">Locations</Link></li>
+</ul>
+<LocationContext.Provider value={{ locationForMap, setLocationForMap }}>
+	<div className="flex">
+		<div className="w-50 pa3 mr2">
+			<LeftPanel locations={mockLocations} />
+		</div>
+		<div className="w-50 pa3 mr2">
+			<RightPanel />
+		</div>
+	</div>
+</LocationContext.Provider> */}
