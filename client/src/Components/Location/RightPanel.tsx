84f6912d65b47c352e52d742e80770c40b638cc6
Jessica Liu <msjessicavliu@gmail.com>
2022-01-14 23:56:55 -0800

    Add delete location button

--- client/src/Components/Location/RightPanel.tsx
+++ client/src/Components/Location/RightPanel.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { MainMap } from './MainMap';
 
 export const RightPanel = () => {
-    const onAddLocationToItineraryClick = () => {
+    const handleAddLocationToItineraryClick = useCallback(() => {
         console.log('location added to itinerary');
-    }
+    }, []);
+
+    const handleDeleteLocation = useCallback(() => {
+        console.log('location deleted');
+    }, []);
 
     return (
         <>
             <MainMap/>
             <br/>
-            <button onClick={onAddLocationToItineraryClick}>Add to itinerary</button>
+            <div>
+                <button onClick={handleAddLocationToItineraryClick}>Add to itinerary</button>
+            </div>
+            <br/>
+            <div>
+                <button onClick={handleDeleteLocation}> Delete Location </button>
+            </div>
         </>
     );
 };
